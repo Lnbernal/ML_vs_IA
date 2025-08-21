4ddbb51c887f62628d8ecf2456337d2780794e46
Nicolas Bernal <Nicolas.Bernal@users.noreply.github.com>
2025-08-20 19:07:43 -0500

    Se creo una rama para la segunda actividad

--- Python/uno.py
+++ Python/uno.py
@@ -3,7 +3,7 @@ import pandas as p
 import flask as f
 print("Numpy: ",np.__version__)
 print("pandas: ",p.__version__)
-print("flask: ",f.__version__)
+
 lista =[1,2,3,4,5]
 arr = np.array(lista)
 print(arr)
